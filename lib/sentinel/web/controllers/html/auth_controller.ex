05d9c8f331b47549233e447409096feb54f1ebce
paul <paul@tymate.com>
2018-02-08 10:06:22 +0100

    translate 'failed to authenticate error'

--- lib/sentinel/web/controllers/html/auth_controller.ex
+++ lib/sentinel/web/controllers/html/auth_controller.ex
@@ -40,7 +40,7 @@ defmodule Sentinel.Controllers.Html.AuthController do
     changeset = Sentinel.Session.changeset(%Sentinel.Session{})
     conn
     |> put_status(401)
-    |> put_flash(:error, "Failed to authenticate")
+    |> put_flash(:error, Gettext.dgettext(Config.gettext_module, "flash_error", "Failed to authenticate"))
     |> render(Config.views.session, "new.html", %{conn: conn, changeset: changeset, providers: Config.ueberauth_providers})
   end
 
